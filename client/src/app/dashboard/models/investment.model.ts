a7288b98125cc0f392a5149776d83d32f94b3275
maxdvv <max.dpkg@gmail.com>
2023-12-08 20:00:03 +0200

    Added: investment service and model

---
+++ client/src/app/dashboard/models/investment.model.ts
@@ -0,0 +1,17 @@
+export interface ServerResponse<T> {
+  status: string;
+  data: T;
+}
+
+export interface CreateInvestmentResponse {
+  userId: string;
+  deposit: number;
+  profit: number;
+  bonus: number
+  availableProfit: number
+  availableBonus: number
+  _id: string;
+  createdAt: string,
+  updatedAt: string,
+}
+
---
+++ client/src/app/dashboard/services/investment.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from "rxjs";
+import { apis } from "../../../environments";
+import { HttpClient } from "@angular/common/http";
+import { CreateInvestmentResponse, ServerResponse } from "../models/investment.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvestmentService {
+  public isMakeInvestment$ = new BehaviorSubject(false);
+  constructor(private http: HttpClient) {}
+
+  public addInvestment(userId: string, deposit: number): Observable<ServerResponse<CreateInvestmentResponse>> {
+    return this.http.post<ServerResponse<CreateInvestmentResponse>>(
+      apis.addInvestment, { userId, deposit }, { withCredentials: true }
+    );
+  }
+
+  public getInvestment(userId: string): Observable<CreateInvestmentResponse[]> {
+    return this.http.get<CreateInvestmentResponse[]>(`${apis.getInvestmentByUserId}/${userId}`,  { withCredentials: true });
+  }
+}
