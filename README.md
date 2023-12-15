b5d755969ff2e04bd4f9cf42ab93ac48871a911d
maxdvv <max.dpkg@gmail.com>
2023-12-15 17:46:55 +0200

    Added: docker-compose file for run redis, redis session store, logger for redisClient

--- README.md
+++ README.md
@@ -58,6 +58,15 @@ $ npm run test:e2e
 $ npm run test:cov
 ```
 
+## Docker
+```bash
+# run redis
+$ docker compose up redis -d
+
+# run the redis CLI
+$ docker compose run rcli
+```
+
 ## Support
 
 Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as redis from 'redis';
+import * as Store from 'connect-redis';
 import * as session from 'express-session';
 import * as passport from 'passport';
 import * as express from 'express';
 import * as path from 'path';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { Logger } from "@nestjs/common";
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const RedisStore = Store(session);
+  const redisClient = redis.createClient({
+    host: String(process.env.REDIS_HOST),
+    port: Number(process.env.REDIS_PORT)
+  });
+  redisClient.on('error', (err) =>
+    Logger.error('Could not establish a connection with redis. ' + err)
+  );
+  redisClient.on('connect', () =>
+    Logger.verbose('Connected to redis successfully')
+  );
 
   app.enableCors({
     origin: 'http://localhost:4200',
@@ -26,15 +41,17 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.use(
+  app.use(
     session({
+      store: new RedisStore({ client: redisClient }),
       secret: process.env.SECRET_SESSION_KEY,
       resave: Boolean(process.env.SESSION_RESAVE),
-      saveUninitialized: Boolean(process.env.SESSION_SAVE_UNINITIALIZED),
-    }),
+      saveUninitialized: Boolean(process.env.SESSION_SAVE_UNINITIALIZED)
+    })
   );
-  await app.use(passport.initialize());
-  await app.use(passport.session())
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
